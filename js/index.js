c7f042b49f62ca09027ba131c66e546bbd831f29
Nana Chuto <mail@nanachuto.com>
2023-01-14 10:03:53 +0100

    wip

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
   function startGame() {}
 };
+
+const ctx = document.getElementById("canvas").getContext("2d");
+const roadImg = new Image();
+imgScale = 500 / 700;
+roadImg.onload = function () {
+  ctx.drawImage(roadImg, 0, 0, 500 * imgScale, 700);
+};
+
+roadImg.src = "./images/road.png";
+
+const carImg = new Image();
+imgScale = 500 / 700;
+carImg.onload = function () {
+  ctx.drawImage(carImg, 141, 440, 100 * imgScale, 150);
+};
+
+carImg.src = "./images/car.png";
